b8d23d9a7a38731ea83df9277dee6ea405a0fd17
ardf <ruyefadf@gmail.com>
2022-11-13 08:35:40 +0530

    Added whitenoise

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'allauth',
     'account',
@@ -73,6 +74,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,6 +84,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'SocialMedia.urls'
+WHITENOISE_USE_FINDERS = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
